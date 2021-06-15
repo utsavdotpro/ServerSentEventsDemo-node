312079e4b46050826cc36b7c502037dbc173b010
Utsav Barnwal <u.barnwal36@gmail.com>
2021-06-16 04:38:39 +0530

    limit stream writes to 100

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const app = require("express")();
 
 const port = process.env.PORT || 8080;
 
+const MAX_WRITES = 100;
+
 app.get("/", (req, res) => res.send("Hello Server-Sent Event"));
 
 app.get("/stream", (req, res) => {
@@ -17,5 +19,12 @@ let i = 0;
 
 function send(res) {
   res.write("data: " + `This is response #${++i}!\n\n`);
+
+  if (i >= MAX_WRITES) {
+    res.end();
+    console.log(`Stream ended after ${i} writes!`);
+    return;
+  }
+
   setTimeout(() => send(res), 1000);
 }
