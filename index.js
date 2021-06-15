c1e5e5f7fc3cccef517e8aa5f406eb9d7d559acd
Utsav Barnwal <u.barnwal36@gmail.com>
2021-06-16 04:05:11 +0530

    create basic SSE on /stream

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ const app = require("express")();
 
 app.get("/", (req, res) => res.send("Hello Server-Sent Event"));
 
+app.get("/stream", (req, res) => {
+  res.setHeader("Content-Type", "text/even-stream");
+  send(res);
+});
+
 app.listen(8080);
 
 console.log("Listening on 8080");
+
+let i = 0;
+
+function send(res) {
+  res.write("data: " + `This is response #${++i}!\n\n`);
+  setTimeout(() => send(res), 1000);
+}
